3184ceb377bc3f74abbaaf624b01b2784445a8e1
zhaozihanzzh <zhaozihanrxh@163.com>
2017-07-20 08:57:22 +0800

    Initial Commit 1

---
+++ MEUISmoothBar/src/main/java/com/meui/SmoothBar/MainActivity.java
@@ -0,0 +1,12 @@
+package com.meui.SmoothBar;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
